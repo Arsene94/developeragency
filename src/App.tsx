e6db071fe94f32fd2a389fe1de9c723b4e3ebed2
Arsene94 <arsene.claudiu94@gmail.com>
2025-06-02 14:24:01 +0300

    Create Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import ServicePage from './pages/ServicePage';
+import LoginPage from './pages/LoginPage';
 import AdminNavbar from './admin/AdminNavbar.tsx';
 import AdminDashboard from './admin/AdminDashboard';
 import AdminServices from './admin/AdminServices';
@@ -20,46 +21,45 @@ import PrivateRoute from "./context/PrivateRoute.tsx";
 
 function HomePage() {
   return (
-      <>
-        <Navbar />
-        <main>
-          <Hero />
-          <Services />
-          <Technologies />
-          <Portfolio />
-          <Process />
-          <Pricing />
-          <Testimonials />
-          <Contact />
-        </main>
-      </>
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Services />
+        <Technologies />
+        <Portfolio />
+        <Process />
+        <Pricing />
+        <Testimonials />
+        <Contact />
+      </main>
+    </>
   );
 }
 
 function App() {
   return (
-      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/" element={<HomePage />} />
-            <Route path="/services/:slug" element={<ServicePage />} />
-            <Route path="zjadminwebarcats/login" element={<AdminLogin />} />
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/services/:slug" element={<ServicePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="zjadminwebarcats/login" element={<AdminLogin />} />
 
-            {/* Admin Routes */}
-              <Route path="/zjadminwebarcats" element={<PrivateRoute />}>
-                  <Route element={<AdminNavbar />}>
-                      <Route index element={<AdminDashboard />} />
-                      <Route path="services" element={<AdminServices />} />
-                      <Route path="portfolio" element={<AdminPortfolio />} />
-                      <Route path="testimonials" element={<AdminTestimonials />} />
-                  </Route>
-              </Route>
-            </Routes>
-          <Footer />
-        </div>
-      </Router>
+          {/* Admin Routes */}
+          <Route path="/zjadminwebarcats" element={<PrivateRoute />}>
+            <Route element={<AdminNavbar />}>
+              <Route index element={<AdminDashboard />} />
+              <Route path="services" element={<AdminServices />} />
+              <Route path="portfolio" element={<AdminPortfolio />} />
+              <Route path="testimonials" element={<AdminTestimonials />} />
+            </Route>
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
-export default App;
